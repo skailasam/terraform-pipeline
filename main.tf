2cac3cdd39a9670ec06c5143b12f0677a28567ce
shafon <fengxiongchen@Gmail.com>
2021-11-16 22:47:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ resource "aws_security_group" "my_security_group" {
   }
 
  ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 2222
+    to_port     = 2222
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
